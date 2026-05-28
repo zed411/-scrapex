98cbe41ee16c5a8bb02326284eeb33828a495ed4
zed411 <79305205+zed411@users.noreply.github.com>
2026-05-28 13:58:20 +1000

    cleanup

--- test_scraper.js
+++
@@ -1,4 +0,0 @@
-const { scrapeGoogleMaps } = require('./scraper');
-scrapeGoogleMaps({ searchString: 'Coffee shops', locationQuery: 'Bushwick', maxResults: 3 })
-  .then(r => console.log(JSON.stringify(r, null, 2)))
-  .catch(e => console.error(e));
